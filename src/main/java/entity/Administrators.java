5504a9b72a616a99fdadb54d5bb1154d294d4af6
lhh <877356622@qq.com>
2021-05-09 22:12:50 +0800

    创建实体类

---
+++ src/main/java/entity/Administrators.java
@@ -0,0 +1,22 @@
+package entity;
+
+public class Administrators {
+    private String a_name;
+    private String a_password;
+
+    public String getA_name() {
+        return a_name;
+    }
+
+    public void setA_name(String a_name) {
+        this.a_name = a_name;
+    }
+
+    public String getA_password() {
+        return a_password;
+    }
+
+    public void setA_password(String a_password) {
+        this.a_password = a_password;
+    }
+}
